17dd239b1998446a0d05647ebdd483d0661efc26
Sheth, Dilan <dilan.sheth19@imperial.ac.uk>
2023-04-16 18:44:30 +0100

    Changed output directory

--- singleFileTest.sh
+++ singleFileTest.sh
@@ -7,7 +7,6 @@ directory="/home/dilan/dafny-verifier"
 cp test0.dfy "$directory/test.dfy"
 
 function errorFound() {
-  echo $1
   if [ $1 -ne 0 ]
   then
     rm -rf test-go || true
@@ -16,7 +15,7 @@ function errorFound() {
     rm -rf test.js || true
     rm -rf test.jar || true
     rm -rf test || true
-    rm -rf outputs || true
+    rm -rf persesOutputs || true
 
     cd $orig
     exit 1
@@ -30,8 +29,8 @@ rm -rf test-py || true
 rm -rf test.js || true
 rm -rf test || true
 
-rm -rf outputs || true
-mkdir outputs
+rm -rf persesOutputs || true
+mkdir persesOutputs
 
 
 javac -cp src/main/java/ -d ./out/ src/main/java/Main/GenerateProgram.java src/main/java/Main/CompareOutputs.java
@@ -44,7 +43,6 @@ errorFound $err
 ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:go /spillTargetCode:3 test.dfy > tmp.txt 2>&1
 if [ $? -eq 0 ]
 then
-  echo "Created Go files"
   mkdir test-go-run
   cp -R test-go/* test-go-run/
   rm -rf test-go || true
@@ -60,12 +58,10 @@ then
     cd ../..
     if [ $? -eq 0 ]
     then
-      ./test-go-run/src/test > "outputs/output-go-$y.txt"
+      ./test-go-run/src/test > "persesOutputs/output-go.txt"
       err=$?
-      errorFound $?
+      errorFound $err
     fi
-  else
-    echo "Failed to generate Go program"
   fi
   rm -rf test-go-run || true
 fi
@@ -74,28 +70,28 @@ fi
 ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:js /compile:2 /compileVerbose:0 test.dfy > tmp.txt  2>&1
 if [ $? -eq 0 ]
 then
-  node test.js > "outputs/output-js.txt"
+  node test.js > "persesOutputs/output-js.txt"
   err=$?
-  errorFound $?
+  errorFound $err
 fi
 
 ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:java /compile:2 /compileVerbose:0 test.dfy > tmp.txt  2>&1
 if [ $? -eq 0 ]
 then
-  java -jar test.jar > "outputs/output-java.txt"
+  java -jar test.jar > "persesOutputs/output-java.txt"
   err=$?
-  errorFound $?
+  errorFound $err
 fi
 
 ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:py /compile:2 /compileVerbose:0 test.dfy > tmp.txt  2>&1
 if [ $? -eq 0 ]
 then
-  python3 test-py/test.py > "outputs/output-py.txt"
+  python3 test-py/test.py > "persesOutputs/output-py.txt"
   err=$?
-  errorFound $?
+  errorFound $err
 fi
 
-java -cp out/ Main.CompareOutputs 1
+java -cp out/ Main.CompareOutputs -1 "persesOutputs/"
 err=$?
 
 rm -rf test-go || true
@@ -103,7 +99,7 @@ rm -rf test-py || true
 rm -rf test.js || true
 rm -rf test.jar || true
 rm -rf test || true
-rm -rf outputs || true
+rm -rf persesOutputs || true
 
 cd $orig
 
