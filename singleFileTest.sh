12db272163e7689eb70c44ce7e91a660cd5ad97c
Sheth, Dilan <dilan.sheth19@imperial.ac.uk>
2023-04-16 17:34:32 +0100

    Change go dafny

--- singleFileTest.sh
+++ singleFileTest.sh
@@ -11,6 +11,7 @@ function errorFound() {
   if [ $1 -ne 0 ]
   then
     rm -rf test-go || true
+    rm -rf test-go-run || true
     rm -rf test-py || true
     rm -rf test.js || true
     rm -rf test.jar || true
@@ -24,6 +25,7 @@ function errorFound() {
 cd $directory
 
 rm -rf test-go || true
+rm -rf test-go-run || true
 rm -rf test-py || true
 rm -rf test.js || true
 rm -rf test || true
@@ -35,17 +37,39 @@ mkdir outputs
 javac -cp src/main/java/ -d ./out/ src/main/java/Main/GenerateProgram.java src/main/java/Main/CompareOutputs.java
 
 
-Dafny /noVerify /compile:3 /compileVerbose:0 test.dfy > tmp.txt  2>&1
+./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compile:3 /compileVerbose:0 test.dfy > tmp.txt  2>&1
 err=$?
 errorFound $err
 
-./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:go /compile:2 /compileVerbose:0 test.dfy > tmp.txt  2>&1
+./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:go /spillTargetCode:3 test.dfy > tmp.txt 2>&1
 if [ $? -eq 0 ]
 then
-  ./test > "outputs/output-go.txt"
-  err=$?
-  errorFound $?
+  echo "Created Go files"
+  mkdir test-go-run
+  cp -R test-go/* test-go-run/
+  rm -rf test-go || true
+  cd test-go-run/src
+  go mod init src  > tmp.txt 2>&1
+  cd ..
+  find ./src \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/_System "System_"/_System "src\/System_"/g'  > tmp.txt 2>&1
+  find ./src \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/_dafny "dafny"/_dafny "src\/dafny"/g'  > tmp.txt 2>&1
+  cd src
+  go build test.go  > tmp.txt 2>&1
+  if [ $? -eq 0 ]
+  then
+    cd ../..
+    if [ $? -eq 0 ]
+    then
+      ./test-go-run/src/test > "outputs/output-go-$y.txt"
+      err=$?
+      errorFound $?
+    fi
+  else
+    echo "Failed to generate Go program"
+  fi
+  rm -rf test-go-run || true
 fi
+
 
 ./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:js /compile:2 /compileVerbose:0 test.dfy > tmp.txt  2>&1
 if [ $? -eq 0 ]
