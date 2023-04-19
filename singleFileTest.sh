f1e483c157ccc5e14a3428ba2d4026eb94f5c2e6
root <root@cloud-vm-42-30.doc.ic.ac.uk>
2023-04-19 17:03:04 +0100

    Update test script

--- singleFileTest.sh
+++ singleFileTest.sh
@@ -2,111 +2,96 @@
 echo "PID: $$"
 trap 'kill -9 $$' SIGINT
 
-orig=$(pwd)
-directory="/home/dilan/dafny-verifier"
-cp test0.dfy "$directory/test.dfy"
-
-function errorFound() {
-  if [ $1 -ne 0 ]
-  then
-    rm -rf test-go || true
-    rm -rf test-go-run || true
-    rm -rf test-py || true
-    rm -rf test.js || true
-    rm -rf test.jar || true
-    rm -rf test || true
-    rm -rf persesOutputs || true
-
-    cd $orig
-    exit 1
-  fi
-}
-cd $directory
-
-rm -rf test-go || true
-rm -rf test-go-run || true
-rm -rf test-py || true
-rm -rf test.js || true
-rm -rf test || true
+source ~/.profile
 
 rm -rf persesOutputs || true
 mkdir persesOutputs
 
+Dafny /noVerify /compile:3 /compileVerbose:0 testPerses.dfy > tmp.txt  # 2>&1
+if [ $? -ne 0 ]
+then
+  exit 1
+fi
+echo "testPerses.dy validated correctly"
 
-javac -cp src/main/java/ -d ./out/ src/main/java/Main/GenerateProgram.java src/main/java/Main/CompareOutputs.java
+cp -r /home/ds1619/dafny_validator .
+cd dafny_validator
+python3 dafny_validator.py ../testPerses.dfy
+if [ $? -ne 0 ]
+then
+    cd ..
+    rm -rf dafny_validator || true
+    echo "testPerses.dfy prints incorrect type"
+    exit 1
+fi
+cd ..
+rm -rf dafny_validator || true
+echo "testPerses.dfy prints correct types"
 
 
-./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compile:3 /compileVerbose:0 test.dfy > tmp.txt  2>&1
-err=$?
-errorFound $err
-
-./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:go /spillTargetCode:3 test.dfy > tmp.txt 2>&1
+Dafny /noVerify /compileTarget:go /spillTargetCode:3 testPerses.dfy > tmp.txt # 2>&1
 if [ $? -eq 0 ]
 then
   mkdir test-go-run
-  cp -R test-go/* test-go-run/
-  rm -rf test-go || true
+  cp -R testPerses-go/* test-go-run/
+  rm -rf testPerses-go || true
   cd test-go-run/src
-  go mod init src  > tmp.txt 2>&1
+  go mod init src > tmp.txt # 2>&1
+  go mod tidy
   cd ..
-  find ./src \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/_System "System_"/_System "src\/System_"/g'  > tmp.txt 2>&1
-  find ./src \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/_dafny "dafny"/_dafny "src\/dafny"/g'  > tmp.txt 2>&1
+  find ./src \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/_System "System_"/_System "src\/System_"/g' > tmp.txt # 2>&1
+  find ./src \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/_dafny "dafny"/_dafny "src\/dafny"/g' > tmp.txt # 2>&1
   cd src
-  go build test.go  > tmp.txt 2>&1
+  go build testPerses.go > tmp.txt # 2>&1
   if [ $? -eq 0 ]
   then
     cd ../..
     if [ $? -eq 0 ]
     then
-      ./test-go-run/src/test > "persesOutputs/output-go.txt"
-      err=$?
-      errorFound $err
+      ./test-go-run/src/testPerses > "persesOutputs/output-go.txt"
+      echo "go file created correctly"
     fi
   fi
   rm -rf test-go-run || true
 fi
 
 
-./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:js /compile:2 /compileVerbose:0 test.dfy > tmp.txt  2>&1
+npm install bignumber.js > tmp.txt # 2>&1
+Dafny /noVerify /compileTarget:js /compile:2 /compileVerbose:0 testPerses.dfy > tmp.txt  # 2>&1
 if [ $? -eq 0 ]
 then
-  node test.js > "persesOutputs/output-js.txt"
-  err=$?
-  errorFound $err
+    node testPerses.js > "persesOutputs/output-js.txt"
+    echo "js file created correctly"
+    rm -rf testPerses.js || true
 fi
 
-./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:java /compile:2 /compileVerbose:0 test.dfy > tmp.txt  2>&1
+Dafny /noVerify /compileTarget:java /compile:2 /compileVerbose:0 testPerses.dfy > tmp.txt  # 2>&1
 if [ $? -eq 0 ]
 then
-  java -jar test.jar > "persesOutputs/output-java.txt"
-  err=$?
-  errorFound $err
+    java -jar testPerses.jar > "persesOutputs/output-java.txt"
+    echo "java file created correctly"
+    rm -rf testPerses.jar || true
+    rm -rf testPerses-java || true
 fi
 
-./src/main/dafny_compiler/dafny/Binaries/Dafny /noVerify /compileTarget:py /compile:2 /compileVerbose:0 test.dfy > tmp.txt  2>&1
+Dafny /noVerify /compileTarget:py /compile:2 /compileVerbose:0 testPerses.dfy > tmp.txt  # 2>&1
 if [ $? -eq 0 ]
 then
-  python3 test-py/test.py > "persesOutputs/output-py.txt"
-  err=$?
-  errorFound $err
+  python3 testPerses-py/testPerses.py > "persesOutputs/output-py.txt"
+  echo "py file created correctly"
+  rm -rf testPerses-py || true
 fi
 
-java -cp out/ Main.CompareOutputs -1 "persesOutputs/"
-err=$?
+for file in persesOutputs/*; do
+    for file2 in persesOutputs/*; do
+	echo "Testing diff between files $file and $file2"
+	diff $file $file2 > tmp.txt
+	if [ $? -ne 0 ]
+	then
+	    echo "diff found between files $file and $file2"
+	    exit 0
+	fi
+    done
+done
 
-rm -rf test-go || true
-rm -rf test-py || true
-rm -rf test.js || true
-rm -rf test.jar || true
-rm -rf test || true
-rm -rf persesOutputs || true
-
-cd $orig
-
-if [ $err -eq 1 ]
-then
-  exit 0
-else
-  exit 1
-fi
-
+exit 1
